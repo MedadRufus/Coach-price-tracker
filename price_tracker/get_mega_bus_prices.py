bbf5b72b3401bd28e5f9e5794eb9e8f6f4cf90b4
Medad Rufus Newman <medad@medadnewman.co.uk>
2022-08-22 19:19:19 +0100

    Create get_mega_bus_prices.py

---
+++ price_tracker/get_mega_bus_prices.py
@@ -0,0 +1,28 @@
+from pprint import PrettyPrinter
+import requests
+
+
+def do_scraping():
+    url = "https://uk.megabus.com/journey-planner/api/journeys?originId=56&destinationId=13&departureDate=2022-09-02&totalPassengers=1&concessionCount=0&nusCount=0&otherDisabilityCount=0&wheelchairSeated=0&pcaCount=0&days=1"
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-GB,en;q=0.5",
+        "Connection": "keep-alive",
+        "Host": "uk.megabus.com",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
+    }
+
+    conn = requests.get(url=url, headers=headers)
+    print(conn.text)
+    # json = conn.json()
+    # print(json)
+
+
+if __name__ == "__main__":
+    do_scraping()
